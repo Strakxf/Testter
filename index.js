952cd20277e54fef08d6f7221bfaa1533848a486
Strak <42559406+Strakxf@users.noreply.github.com>
2018-08-23 19:35:32 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * @file The starting point of Bastion
+ * @author SonnyK (a.k.a Strak)
+ * @license GPL-3.0
+ */
+
+const Tesseract = xrequire('tesseract');
+const credentials = xrequire('./settings/credentials.json');
+const configurations = xrequire('./settings/config.json');
+const Manager = new Tesseract.ShardingManager('./bastion.js', {
+  totalShards: configurations.shardCount,
+  token: credentials.token
+});
+const log = xrequire('./handlers/logHandler');
+
+Manager.spawn();
+
+Manager.on('launch', shard => {
+  log.info(`Launching Shard ${shard.id} [ ${shard.id + 1} of ${Manager.totalShards} ]`);
+});
